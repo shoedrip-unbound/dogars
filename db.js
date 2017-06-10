ad9c5578caceeb66fa410a4d358a168c2971567d
shoedrip <shoedrip@users.noreply.github.com>
2017-06-10 23:10:44 +0200

    Remove callback hell

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ let Client   = require('mysql');
 let fs       = require('fs');
 let poke     = require('./poke-utils');
 let tripcode = require('tripcode');
-
 let settings = JSON.parse(fs.readFileSync('settings.json'));
+let p        = require('util').promisify;
 
 let c = Client.createConnection(settings.db);
 
@@ -11,154 +11,90 @@ let total = 0;
 
 c.query('SELECT COUNT(*) FROM Sets', (e, rows) => {
 	if (e)
-		console.log(e);
+		throw e;
 	total = rows[0]['COUNT(*)'];
 	total = parseInt(total);
 	module.exports.total = total;
 });
 
-module.exports.getAllSets = (cb) => {
-	c.query('select * from Sets', (e, rows) => {
-		if (e)
-			console.log(e);
-		cb(rows);
-	});
-}
+let getAllSets = async => c.query('select * from Sets');
 
-module.exports.memesInReplay = (rid, cb) => {
-	c.query('SELECT * FROM memes.Sets where id in (SELECT idset from memes.sets_in_replays where idreplay = ?)', [rid], (e, rows) => {
-		cb(e, rows);
-	});
-}
+let memesInReplay = p((rid, cb) => {
+	c.query('select * from memes.Sets where id in (select idset from memes.sets_in_replays where idreplay = ?)', [rid], cb);
+});
 
-module.exports.getReplays = (cb) => {
-	c.query('select * from replays order by id desc;', (e, rows) => {
-		if (e)
-			console.log(e);
-		cb(rows);
-	});
-}
+let getReplays = p((cb) => {
+	c.query('select * from replays order by id desc;', cb);
+})
 
-module.exports.getChampFromTrip = (trip, cb) => {
-	c.query('select * from memes.champs where trip = ? order by id desc;', [trip],
-			(e, rows) => {
-				if (e)
-					console.log(e);
-				cb(rows);
-			});
-}
+let getChampFromTrip = p((trip, cb) => {
+	c.query('select * from memes.champs where trip = ? order by id desc;',
+			[trip],
+			cb);
+})
 
-module.exports.getChamps = (cb) => {
-	c.query('select * from memes.champs order by wins desc;',
-			(e, rows) => {
-				if (e)
-					console.log(e);
-				console.log(rows);
-				cb(rows);
-			});
-}
+let getChamps = p(cb => {
+	c.query('select * from memes.champs order by wins desc;', cb);
+})
 
-module.exports.createChampFromTrip = (name, trip, cb) => {
-	c.query('insert into memes.champs (name, trip) values (?, ?) ', [name || '', trip],
-			(e, rows) => {
-				if (e)
-					console.log(e);
-				cb(rows);
-			});
-}
+let createChampFromTrip = p((name, trip, cb) => {
+	c.query('insert into memes.champs (name, trip) values (?, ?) ', [name || '', trip], cb);
+})
 
-module.exports.addReplay = (data, cb) => {
-	c.query('insert into replays (link, description) values (?, ?);', [data.link, data.description] ,(e, rows) => {
-		if (e)
-			console.log(e);
-		cb(rows);
-	});
-}
+let addReplay = p((data, cb) => {
+	c.query('insert into replays (link, description) values (?, ?);', [data.link, data.description] , cb);
+})
 
-module.exports.getSetsPage = (setPerPage, pageNumber, cb) => {
+let getSetsPage = p((setPerPage, pageNumber, cb) => {
 	let offset = setPerPage * pageNumber;
-	c.query('select * from Sets order by id desc limit ? offset ?;', [~~setPerPage, ~~offset], (e, rows) => {
-		if (e)
-			console.log(e);
-		if(cb)
-			cb(rows);
-	});
-}
+	c.query('select * from Sets order by id desc limit ? offset ?;', [~~setPerPage, ~~offset], cb);
+})
 
-module.exports.addSetToReplay = (setid, rid) => {
-	c.query('insert into memes.sets_in_replays (idreplay, idset) values (?, ?)',
-			[rid, setid], (e, rows) => {
-				if (e)
-					console.log(e);
-			});
-}
+let addSetToReplay = p((setid, rid, cb) => {
+	c.query('insert into memes.sets_in_replays (idreplay, idset) values (?, ?)', [rid, setid], cb);
+})
 
-module.exports.updateChampAvatar = (trip, aid) => {
+let updateChampAvatar = p((trip, aid, cb) => {
 	c.query('update memes.champs set avatar = ? where trip = ?',
-			[aid, trip], (e, rows) => {
-				if (e)
-					console.log(e);
-				console.log(aid + 'UPDATED AVATAR FOR ' + trip);
-			});
-}
+			[aid, trip], cb);
+})
 
-module.exports.registerChampResult = (battleData, hasWon) => {
+let registerChampResult = p(async (battleData, hasWon, cb) => {
 	let replayurl;
 
 	if (hasWon) {
 		poke.saveReplay(battleData.champ.champ_battle, () => {
 			console.log('replay saved');
-		})
+		});
 		replayurl = 'http://replay.pokemonshowdown.com/' + battleData.roomid;
 	}
 
 	let inc = hasWon ? 'wins' : 'loses';
-	console.log('===================' + inc + '=================');
-
-	let meendthisshit = () => c.query('update memes.champs set ' + inc + ' = ' + inc + ' + 1 where trip = ?',
-									  [battleData.champ.champ_trip], (e, rows) => {
-										  if (e)
-											  console.log(e);
-										  console.log(battleData)
-										  module.exports.updateChampAvatar(battleData.champ.champ_trip,
-															   battleData.champ.avatar.substr(battleData.champ.avatar[0] == '#'));
-										  if (!hasWon)
-											  return;
-										  module.exports.addReplay({
-											  link: replayurl,
-											  description: 'Automatically uploaded replay. Champ: ' + battleData.champ.champ_name + ' ' + battleData.champ.champ_trip
-										  }, info => {
-											  for(var i = 0; i < battleData.memes.length; ++i) {
-												  module.exports.getSetsByPropertyExact({name: battleData.memes[i].name}, (sets) => {
-													  if (sets.length < 1)
-														  return;
-													  sets = sets[0];
-													  module.exports.addSetToReplay(sets.id, info.insertId);
-												  });
-											  }
-										  });
-									  });
-	module.exports.getChampFromTrip(battleData.champ.champ_trip, (champ) => {
-		if (champ.length == 0) {
-			module.exports.createChampFromTrip(battleData.champ.champ_name, battleData.champ.champ_trip,
-											   (row) => {
-												   meendthisshit();
-											   })
-		}
-		else
-			meendthisshit();
-	})
-}
-
-module.exports.getSetById = (id, cb) => {
-	c.query('select * from Sets where id = ?', [id], (e, rows) => {
-		if (e)
-			console.log(e);
-		cb(rows[0]);
+	let champ = getChampFromTrip(battleData.champ.champ_trip);
+	if (champ.length == 0)
+		createChampFromTrip(battleData.champ.champ_name, battleData.champ.champ_trip)
+	await c.query('update memes.champs set ' + inc + ' = ' + inc + ' + 1 where trip = ?', [battleData.champ.champ_trip]);
+	updateChampAvatar(battleData.champ.champ_trip, battleData.champ.avatar.substr(battleData.champ.avatar[0] == '#'));
+	if (!hasWon)
+		return;
+	let info = await addReplay({
+		link: replayurl,
+		description: 'Automatically uploaded replay. Champ: ' + battleData.champ.champ_name + ' ' + battleData.champ.champ_trip
 	});
-}
+	for(var i = 0; i < battleData.memes.length; ++i) {
+		let sets = await getSetsByPropertyExact({name: battleData.memes[i].name});
+		if (sets.length < 1)
+			return;
+		sets = sets[0];
+		addSetToReplay(sets.id, info.insertId, cb);
+	}
+})
 
-module.exports.getSetsByProperty = (props, cb) => {
+let getSetById = p((id, cb) => {
+	c.query('select * from Sets where id = ?', [id], cb);
+})
+
+let getSetsByProperty = p((props, cb) => {
 	let querystr = 'select * from Sets where ';
 	let data = [];
 	for(var i in props) {
@@ -167,14 +103,10 @@ module.exports.getSetsByProperty = (props, cb) => {
 		data.push('%' + props[i] + '%');
 	}
 	querystr = querystr.substr(0, querystr.length - 5);
-	c.query(querystr, data, (e, rows) => {
-		if (e)
-			console.log(e);
-		cb(rows);
-	});
-}
+	c.query(querystr, data, cb);
+})
 
-module.exports.getSetsByPropertyExact = (props, cb) => {
+let getSetsByPropertyExact = p((props, cb) => {
 	let querystr = 'select * from Sets where ';
 	let data = [];
 	for(var i in props) {
@@ -183,26 +115,18 @@ module.exports.getSetsByPropertyExact = (props, cb) => {
 		data.push(props[i]);
 	}
 	querystr = querystr.substr(0, querystr.length - 5);
-	c.query(querystr, data, (e, rows) => {
-		if (e)
-			console.log(e);
-		cb(rows);
-	});
-}
+	c.query(querystr, data, cb);
+})
 
-module.exports.getSetsByName = (name, cb) => {
+let getSetsByName = p((name, cb) => {
 	let pattern = '%' + name + '%';
 	c.query('select * from Sets where name like ? or species like ? or move_1 like ? or move_2 like ? or move_3 like ? or move_4 like ?',
 			[pattern,
 			 pattern, pattern, pattern, pattern,
-			 pattern], (e, rows) => {
-				 if (e)
-					 console.log(e);
-				 cb(rows);
-			 });
-}
+			 pattern], cb);
+})
 
-module.exports.createNewSet = (request, cb) => {
+let createNewSet = p((request, cb) => {
 	let row = {};
 	row.hash = tripcode(request.body.trip);
 	row.format = "gen7ou";
@@ -224,17 +148,13 @@ module.exports.createNewSet = (request, cb) => {
 	catch(e) {
 		return cb(e);
 	}
-	console.log(row);
 	let data = ['date_added', 'format', 'creator', 'hash', 'name', 'species',
 				'gender', 'item', 'ability', 'shiny', 'level', 'happiness', 'nature',
 				'move_1', 'move_2', 'move_3', 'move_4', 'hp_ev', 'atk_ev', 'def_ev',
 				'spa_ev', 'spd_ev', 'spe_ev', 'hp_iv', 'atk_iv', 'def_iv', 'spa_iv',
 				'spd_iv', 'spe_iv', 'description'];
-
 	let data_arr = [];
-
 	let querystr = 'INSERT INTO Sets (';
-
 	data.forEach(attr => {
 		data_arr.push(row[attr]);
 		querystr += attr;
@@ -251,72 +171,90 @@ module.exports.createNewSet = (request, cb) => {
 	c.query(querystr, data_arr, (e, rows) => {
 		if (e)
 			return cb(e);
-		cb(null, rows);
 		module.exports.total++;
+		cb(null, rows);
 	});
-}
+})
 
-module.exports.updateSet = (request, cb) => {
-	module.exports.getSetById(request.params.id, row => {
-		if (request.body.trip == '' || (request.body.trip != settings.admin_pass && row.hash != tripcode(request.body.trip)))
-			return cb('Wrong tripcode');
-		row.format = "gen7ou";
-		let formats = ["gen7ou", "gen7anythinggoes", "ubers", "uu", "ru",
-					   "nu", "pu", "lc", "cap"];
-		formats.forEach(f => {
-			if (request.body.format == f)
-				row.format = f;
-		});
-		row.description = request.body.desc.substr(0, 230);
-		row.date_added = +new Date();
-		try {
-			let pok = poke.parseSet(request.body.set);
-			for(var i in pok)
-				row[i] = pok[i];
-		}
-		catch(e) {
+let updateSet = p(async (request, cb) => {
+	let row = await getSetById(request.params.id);
+	row = row[0];
+	if (request.body.trip == '' || (request.body.trip != settings.admin_pass && row.hash != tripcode(request.body.trip)))
+		return cb('Wrong tripcode');
+	
+	row.format = "gen7ou";
+	let formats = ["gen7ou", "gen7anythinggoes", "ubers", "uu", "ru",
+				   "nu", "pu", "lc", "cap"];
+	formats.forEach(f => {
+		if (request.body.format == f)
+			row.format = f;
+	});
+	row.description = request.body.desc.substr(0, 230);
+	row.date_added = +new Date();
+
+	try {
+		let pok = poke.parseSet(request.body.set);
+		for(var i in pok)
+			row[i] = pok[i];
+	}
+	catch(e) {
+		return cb(e);
+	}
+	let data = ['date_added', 'format', 'creator', 'hash', 'name', 'species',
+				'gender', 'item', 'ability', 'shiny', 'level', 'happiness', 'nature',
+				'move_1', 'move_2', 'move_3', 'move_4', 'hp_ev', 'atk_ev', 'def_ev',
+				'spa_ev', 'spd_ev', 'spe_ev', 'hp_iv', 'atk_iv', 'def_iv', 'spa_iv',
+				'spd_iv', 'spe_iv', 'description'];
+
+	let data_arr = [];
+
+	let querystr = 'UPDATE Sets SET ';
+
+	data.forEach(attr => {
+		data_arr.push(row[attr]);
+		querystr += attr + ' = ?';
+		if (attr != 'description')
+			querystr += ', ';
+	})
+	querystr += ' WHERE id = ?';
+	data_arr.push(request.params.id);
+	c.query(querystr, data_arr, (e, rows) => {
+		if (e)
 			return cb(e);
-		}
-		let data = ['date_added', 'format', 'creator', 'hash', 'name', 'species',
-					'gender', 'item', 'ability', 'shiny', 'level', 'happiness', 'nature',
-					'move_1', 'move_2', 'move_3', 'move_4', 'hp_ev', 'atk_ev', 'def_ev',
-					'spa_ev', 'spd_ev', 'spe_ev', 'hp_iv', 'atk_iv', 'def_iv', 'spa_iv',
-					'spd_iv', 'spe_iv', 'description'];
-
-		let data_arr = [];
-
-		let querystr = 'UPDATE Sets SET ';
-
-		data.forEach(attr => {
-			data_arr.push(row[attr]);
-			querystr += attr + ' = ?';
-			if (attr != 'description')
-				querystr += ', ';
-		})
-		querystr += ' WHERE id = ?';
-		data_arr.push(request.params.id);
-		console.log(querystr);
-		c.query(querystr, data_arr, (e, rows) => {
-			if (e)
-				cb(e);
-			else
-				cb(null, rows.info);
-		});
+		cb(null, rows.info);
 	});
-}
+})
 
-module.exports.deleteSet = (request, cb) => {
-	module.exports.getSetById(request.params.id, row => {
-		console.log('TRIP: ' + request.body.trip);
-		if (request.body.trip == '' ||
-			(request.body.trip != settings.admin_pass &&
-			 row.hash != tripcode(request.body.trip)))
-			return cb('Wrong tripcode');
-		c.query('DELETE FROM Sets WHERE id = ?', [request.params.id], (e, rows) => {
-			if (e)
-				return cb(e);
-			cb(null, rows.info);
-			module.exports.total--;
-		});
+let deleteSet = p(async (request, cb) => {
+	let row = await getSetById(request.params.id);	
+	row = row[0];
+	if (request.body.trip == '' ||
+		(request.body.trip != settings.admin_pass &&
+		 row.hash != tripcode(request.body.trip)))
+		return cb('Wrong tripcode');
+	c.query('DELETE FROM Sets WHERE id = ?', [request.params.id], (e, rows) => {
+		if (e)
+			return cb(e);
+		cb(null, rows.info);
+		module.exports.total--;
 	});
-}
+})
+
+module.exports.getAllSets				= getAllSets;
+module.exports.memesInReplay			= memesInReplay;
+module.exports.getReplays				= getReplays;
+module.exports.getChampFromTrip			= getChampFromTrip;
+module.exports.getChamps				= getChamps;
+module.exports.createChampFromTrip		= createChampFromTrip;
+module.exports.addReplay				= addReplay;
+module.exports.getSetsPage				= getSetsPage;
+module.exports.addSetToReplay			= addSetToReplay;
+module.exports.updateChampAvatar		= updateChampAvatar;
+module.exports.registerChampResult		= registerChampResult;
+module.exports.getSetById				= getSetById;
+module.exports.getSetsByProperty		= getSetsByProperty;
+module.exports.getSetsByPropertyExact	= getSetsByPropertyExact;
+module.exports.getSetsByName			= getSetsByName;
+module.exports.createNewSet				= createNewSet;
+module.exports.updateSet				= updateSet;
+module.exports.deleteSet				= deleteSet;
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 let fs       = require('fs');
-let _        = require('async');
+let _        = require('promise-async');
 let tripcode = require('tripcode');
 let poke     = require('./poke-utils');
 let db       = require('./db.js');
@@ -13,7 +13,7 @@ let cookieParser	= require('cookie-parser')
 let bodyParser		= require('body-parser');
 let multer			= require('multer');
 let express			= require('express');
-let upload = multer({dest: '/tmp'});
+let upload          = multer({dest: '/tmp'});
 let router			= express();
 let compression     = require('compression');
 
@@ -60,6 +60,14 @@ let render = (view, data) => {
 	return mustache.render(fileCache['shell'], data, subs);
 }
 
+let sendTemplate = (req, res, n, data) => {
+	data = data || {};
+	res.set({'Content-type': 'text/html'});
+	data = extend(data, genericData(req));
+	res.send(render(n, data));
+	res.end();
+}
+
 let cookie2obj = (str) => {
 	let cook = str.split(';').map(e => e.trim());
 	let ret = {};
@@ -70,15 +78,15 @@ let cookie2obj = (str) => {
 	return ret;
 }
 
-let getSetOfTheDay = cb => {
+let getSetOfTheDay = async cb => {
 	let today = new Date();
 	let seed = today.getDate() * (today.getMonth() + 1) * (today.getYear() + 1900);
 	seed = seed % db.total;
 	// >set of the "day"
 	// >changes everytime you add or delete a set
-	db.getSetById('' + seed, set => {
-		cb(poke.formatSetFromRow(set))
-	});
+	let set = db.getSetById('' + seed);
+	set = await set;
+	return poke.formatSetFromRow(set[0]);
 }
 
 let getCookieData = request => {
@@ -105,82 +113,65 @@ let genericData = (request) => {
 
 router.use(express.static('./public'));
 
-router.get("/", (request, response) => {
-	getSetOfTheDay(set => {
-		set = extend(set, genericData(request));
-		response.set({'Content-type': 'text/html'});
-		response.send(render('index', set));
-		response.end();
-	});
+router.get("/", async (request, response) => {
+	let set = await getSetOfTheDay();
+	sendTemplate(request, response, 'index', set);
 });
 
-router.get("/all", (request, response) => {
+router.get("/all", async (request, response) => {
 	let spp = 15; //request.query.spp || 10;
 	let npages = ~~(db.total / spp) + (db.total % spp != 0);
 	let page = request.query.page || 0;
 	page = ~~page;
-	db.getSetsPage(spp, page, sets => {
-		sets = sets.map(e => { return poke.formatSetFromRow(e)});
-		response.set({'Content-type': 'text/html'});
-		let data = extend({sets: sets}, genericData(request));
-		data = extend(data, {display_pages: true, current_page: ~~page + 1, npages: npages, lastpage: npages - 1});
-		if (page > 0) {
-			data.prev = ~~page - 1;
-			data.has_prev = true;
-		}
-		if (page + 1 < npages)
-			data.next = ~~page + 1;
-		response.send(render('all', data));
-		response.end();
-	});
+	let sets = await db.getSetsPage(spp, page);
+	sets = sets.map(e => { return poke.formatSetFromRow(e)});
+	let data = {sets: sets};
+	data = extend(data, {display_pages: true, current_page: ~~page + 1, npages: npages, lastpage: npages - 1});
+	if (page > 0) {
+		data.prev = ~~page - 1;
+		data.has_prev = true;
+	}
+	if (page + 1 < npages)
+		data.next = ~~page + 1;
+	sendTemplate(request, response, 'all', data);
 });
 
 router.get("/import", (request, response) => {
-	response.set({'Content-type': 'text/html'});
-	let data = genericData(request);
-	response.send(render('import', data));
-	response.end();
+	sendTemplate(request, response, 'import');
 });
 
 router.get("/thanks", (request, response) => {
-	response.set({'Refresh': '2; url=/', 'Content-type': 'text/html'});
-	let data = genericData(request);
-	response.send(render('thanks', data));
-	response.end();
+	response.set({'Refresh': '2; url=/'});
+	sendTemplate(request, response, 'import');
 });
 
-router.post("/update/:id", (request, response, next) => {
+router.post("/update/:id", async (request, response, next) => {
 	let handleErrorGen = e => {
 		if(e) {
-			response.set({'Refresh': '2; url=/'});
-			response.send('You fucked up something. Back to the homepage in 2, 1...');
-			response.end();
-			return;
 		}
-		response.set({'Refresh': '0; url=/set/' + request.params.id});
-		response.end();    
 	};
 
-	if (request.body.action == "Update")
-		db.updateSet(request, handleErrorGen);
-	else if (request.body.action == "Delete")
-		db.deleteSet(request, handleErrorGen);
-	else
-		handleErrorGen(true);
+	try {
+		if (request.body.action == "Update")
+			await db.updateSet(request);
+		else if (request.body.action == "Delete")
+			await db.deleteSet(request);
+		response.set({'Refresh': '0; url=/set/' + request.params.id});
+	}
+	catch(e) {
+		response.set({'Refresh': '2; url=/'});
+		response.send('You fucked up something. Back to the homepage in 2, 1...');
+	}
+	finally {
+		response.end();    
+	}
 });
 
-router.post("/add", (request, response) => {
+router.post("/add", async (request, response) => {
 	try {
-		db.createNewSet(request, (e, info) => {
-			if(e) {
-				response.set({'Refresh': '2; url=/'});
-				response.send('You fucked up something. Back to the homepage in 2, 1...');
-				response.end();
-				return;
-			}
-			response.set({'Refresh': '0; url=/set/' + info.insertId});
-			response.end();
-		});
+		let info = await db.createNewSet(request);
+		response.set({'Refresh': '0; url=/set/' + info.insertId});
+		response.end();
 	}
 	catch(e) {
 		console.log(e);
@@ -197,19 +188,15 @@ router.get("/random", (request, response) => {
 	response.end();
 });
 
-router.post("/search", (request, response) => {
+router.post("/search", async (request, response) => {
 	for(var i in request.body)
 		if(request.body[i] === '')
 			delete request.body[i];
-	console.log(request.body);
-	if(request.body.q)
-		db.getSetsByName(request.body.q, sets => {
-			sets = sets.map(poke.formatSetFromRow);
-			let data = extend({sets: sets}, genericData(request));
-			response.set({'Content-type': 'text/html'});
-			response.send(render('all', data));
-			response.end();
-		});
+	if(request.body.q) {
+		let sets = await db.getSetsByName(request.body.q)
+		sets = sets.map(poke.formatSetFromRow);
+		sendTemplate(request, response, 'all', {sets: sets});
+	}
 	else { // Advanced search
 		let data = ['date_added', 'format', 'creator', 'hash', 'name', 'species',
 					'gender', 'item', 'ability', 'shiny', 'level', 'happiness', 'nature',
@@ -220,132 +207,90 @@ router.post("/search", (request, response) => {
 			if (data.indexOf(i) == -1)
 				delete request.body[i];
 		if (request.body == {}) {
-			let data = extend({sets: sets}, genericData(request));
-			response.set({'Content-type': 'text/html'});
-			response.send(render('all', data));
-			response.end();
+			sendTemplate(request, response, 'all', {sets: []});
 		} else {
-			db.getSetsByProperty(request.body, sets => {
-				sets = sets.map(e => { return poke.formatSetFromRow(e)});
-				let data = extend({sets: sets}, genericData(request));
-				response.set({'Content-type': 'text/html'});
-				response.send(render('all', data));
-				response.end();
-			});
+			let sets = await db.getSetsByProperty(request.body);
+			sets = sets.map(e => { return poke.formatSetFromRow(e)});
+			sendTemplate(request, response, 'all', {sets: sets});			
 		}
 	}
 });
 
-router.get("/search", (request, response) => {
-	if(request.query.q)
-		db.getSetsByName(request.body.q, sets => {
-			sets = sets.map(poke.formatSetFromRow);
-			let data = extend({sets: sets}, genericData(request));
-			response.set({'Content-type': 'text/html'});
-			response.send(render('all', data));
-			response.end();
-		});
+router.get("/search", async (request, response) => {
+	if(request.query.q) {
+		let sets = await db.getSetsByName(request.body.q);
+		sets = sets.map(poke.formatSetFromRow);
+		sendTemplate(request, response, 'all', {sets: sets});
+	}
 	else {
-		response.set({'Content-type': 'text/html'});
-		let data = genericData(request);
-		response.send(render('search', data));
-		response.end();
+		sendTemplate(request, response, 'search', {});
 	}
 });
 
-router.get("/replays", (request, response) => {
-	db.getReplays(replays => {
-		_.map(replays.map(r => r.id), db.memesInReplay, (e, memes) => {
-			for(var i = 0; i < replays.length; ++i)
-				replays[i] = extend(replays[i], {memes: memes[i].map(poke.formatSetFromRow)});
-			let data = extend({replays: replays}, genericData(request));
-			if (request.query.fail)
-				data['error'] = true;
-			response.set({'Content-type': 'text/html'});
-			response.send(render('replays', data));
-			response.end();
-		});
-	});
+router.get("/replays", async (request, response) => {
+	let replays = await db.getReplays();
+	let memes = [];
+	for(var i = 0; i < replays.length; ++i)
+		memes.push(await db.memesInReplay(replays[i].id));
+	replays = replays.map((r, i) => extend(r, {memes: memes[i].map(poke.formatSetFromRow)}));
+	let data = {replays: replays};
+	if (request.query.fail)
+		data['error'] = true;
+	sendTemplate(request, response, 'replays', data);
 });
 
 router.get("/replays/add/:id", (request, response) => {
-	response.set({'Content-type': 'text/html'});
-	let data = genericData(request);
-	data.id = request.params.id;
-	response.send(render('addrset', data));
-	response.end();
+	data = { id: request.params.id };
+	sendTemplate(request, response, 'addrset', data);
 });
 
-router.post("/replays/add/:id", (request, response) => {
+router.post("/replays/add/:id", async (request, response) => {
 	response.set({'Content-type': 'text/html'});
 	response.set({'Refresh': '0; url=/replays'});
 	response.end();
 
 	let id = request.body.set.match(/http:\/\/dogars\.ml\/set\/([0-9]+)/)[1];
-	db.addSetToReplay(id, request.params.id)
+	db.addSetToReplay(id, request.params.id);
 });
 
-router.post("/replays", (request, response) => {
-	let error = false;
-	let disp = replays => {
-		let data = extend({replays: replays}, genericData(request));
-		if(error)
-			data['error'] = true;
-		response.set({'Content-type': 'text/html'});
-		response.send(render('replays', data));
-		response.end();
-	};
-	if(/https?:\/\/replay.pokemonshowdown.com\/(.*)-[0-9]*/.test(request.body.link))
-		db.addReplay(request.body, (e) => {
-			response.set({'Refresh': '0; url=/replays'});
-			response.end();
-		});
+router.post("/replays", async (request, response) => {
+	if(/https?:\/\/replay.pokemonshowdown.com\/(.*)-[0-9]*/.test(request.body.link)) {
+		db.addReplay(request.body);
+		response.set({'Refresh': '0; url=/replays'});
+	}
 	else {
 		response.set({'Refresh': '0; url=/replays?fail=true'});
-		response.end();
 	}
+	response.end();
 });
 
-router.get("/fame", (request, response) => {
-	db.getSetsByProperty({has_custom: 1}, sets => {
-		sets = sets.map(e => { return poke.formatSetFromRow(e)});
-		let data = extend({sets: sets}, genericData(request));
-		response.set({'Content-type': 'text/html'});
-		response.send(render('fame', data));
-		response.end();
-	})
+router.get("/fame", async (request, response) => {
+	let sets = await db.getSetsByProperty({has_custom: 1})
+	sets = sets.map(e => { return poke.formatSetFromRow(e)});
+	sendTemplate(request, response, 'fame', {sets: sets});
 });
 
-router.get("/champs", (request, response) => {
-	db.getChamps(champs => {
-		let data = extend({champs: champs}, genericData(request));
-		response.set({'Content-type': 'text/html'});
-		response.send(render('champs', data));
-		response.end();
-	})
+router.get("/champs", async (request, response) => {
+	let champs = await db.getChamps();
+	let data = {champs: champs};
+	sendTemplate(request, response, 'champs', data);
 });
 
-router.get("/suggest/:type", (request, response) => {
-	let data = genericData(request);
+router.get("/suggest/:type", async (request, response) => {
+	let data = {};
 	if (request.params.type == 'banner') {
-		response.set({'Content-type': 'text/html'});
-		response.send(render('suggest-banner', data));
-		response.end();
+		sendTemplate(request, response, 'suggest-banner');
 	}
 	else if (/^\d+$/.test(request.params.type)) {
-		db.getSetById(request.params.type, set => {
-			if (!set)
-			{
-				response.set({'Refresh': '0; url=/'});
-				response.end();
-				return;
-			}
-			set = poke.formatSetFromRow(set);
-			set = extend(set, data);
-			response.set({'Content-type': 'text/html'});
-			response.send(render('suggest-set', set));
+		let set = await db.getSetById(request.params.type);
+		set = set[0];
+		if (!set) {
+			response.set({'Refresh': '0; url=/'});
 			response.end();
-		});
+			return;
+		}
+		set = poke.formatSetFromRow(set);
+		sendTemplate(request, response, 'suggest-set', set);
 	}
 	else
 		router._404(request, response, '/suggest/' + request.params.type);
@@ -360,7 +305,7 @@ router.post("/suggest", upload.single('sugg'), (request, response, next) => {
 				mkdirp.sync(dir);
 			fs.readdir(dir, (e, f) => {
 				if (e)
-					return console.log(e);
+					throw e;
 				mv(request.file.path, dir + '/' + f.length + '-' + request.file.originalname, {mkdirp: true});
 			});
 		});
@@ -380,20 +325,16 @@ router.post("/suggest", upload.single('sugg'), (request, response, next) => {
 		router._404(request, response, '/suggest/' + request.params.type);
 });
 
-router.get("/set/:id", (request, response) => {
-	db.getSetById(request.params.id, set => {
-		if (!set)
-		{
-			response.set({'Refresh': '0; url=/'});
-			response.end();
-			return;
-		}
-		set = poke.formatSetFromRow(set);
-		set = extend(set, genericData(request));
-		response.set({'Content-type': 'text/html'});
-		response.send(render('set', set));
+router.get("/set/:id", async (request, response) => {
+	let set = await db.getSetById(request.params.id);
+	set = set[0];
+	if (!set) {
+		response.set({'Refresh': '0; url=/'});
 		response.end();
-	});
+		return;
+	}
+	set = poke.formatSetFromRow(set);
+	sendTemplate(request, response, 'set', set);
 });
 
 router._404 = (request, response, path) => {
