442f5c25155614711328e5491d525c68ebc982b9
shoedrip <shoedrip-unbound@dogars.ml>
2019-07-21 22:49:08 +0200

    Handle yet another PS Error...

--- src/Showdown/PSConnection.ts
+++ src/Showdown/PSConnection.ts
@@ -53,7 +53,7 @@ export class PSConnection {
 					let handled = false;
 					// remove messages that where handled by a request
 					mesgs = mesgs.filter(e => {
-						if (e[0] == 'error') {
+						if (e[0] == 'error' || e[1].startsWith('|modal|Your IP')) {
 							this.errored = true;
 							r.rej(e[1]);
 							return false;
