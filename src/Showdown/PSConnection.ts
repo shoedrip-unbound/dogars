89538c0cbab73b9b1ef54f29657f9872d8bb5290
shoedrip <shoedrip-unbound@dogars.ml>
2019-08-30 02:22:40 +0200

    Disable trash

--- src/Showdown/PSConnection.ts
+++ src/Showdown/PSConnection.ts
@@ -50,11 +50,6 @@ export class PSConnection {
 					let handled = false;
 					// remove messages that where handled by a request
 					mesgs = mesgs.filter(e => {
-						if (e[0] == 'error' || (e[1] && e[1].startsWith('|modal|Your IP'))) {
-							this.errored = true;
-							r.rej(e[1]);
-							return false;
-						}
 						let h = r.req.isResponse(e);
 						if (h) {
 							handled = true;
