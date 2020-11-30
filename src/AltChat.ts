e7836406218c17d8b11e4359a377d91b6f965dfb
shoedrip <shoedrip@dogars.ml>
2020-11-30 02:20:21 +0100

    Fix a mistake in /me message

--- src/AltChat.ts
+++ src/AltChat.ts
@@ -113,12 +113,12 @@ class Room {
 
         const inc = tinc[id] || 125;
         timo[id] = now + inc;
-
+        const prefix = `|c|${cli.mark}${cli.name}|/me uploaded a picture:\n|raw|`
         let entry: string;
         if (nsfw)
-            entry = `|c|${cli.mark}${cli.name}| /me uploaded a picture:\n|raw| <details ontoggle="this.children[1].src = '${surl}';"><summary>Image (NSFW)</summary><img style="max-width: 400px; max-height: 400px;"/></details>`;
+            entry = `${prefix} <details ontoggle="this.children[1].src = '${surl}';"><summary>Image (NSFW)</summary><img style="max-width: 400px; max-height: 400px;"/></details>`;
         else
-            entry = `|c|${cli.mark}${cli.name}| /me uploaded a picture:\n|raw| <details open><summary>Image (Worksafe)</summary><img src="${surl}" style="max-width: 400px; max-height: 400px;"/></details>`;
+            entry = `${prefix} <details open><summary>Image (Worksafe)</summary><img src="${surl}" style="max-width: 400px; max-height: 400px;"/></details>`;
         Object.values(this.clients).forEach(c => c.connection.write(`>${this.id}\n${entry}`))
         this.log.push(entry);
     }
